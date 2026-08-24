95e7422e81989915cc3425e70711c0dc0b5bfda6
eoshipnyagov <eoshipnyagov@ya.ru>
2026-08-24 15:48:23 +0500

    Capture secondary VEGA HID control exchange

--- tools/hid_feature_capture.py
+++ tools/hid_feature_capture.py
@@ -23,9 +23,14 @@ function hookBufferFunction(moduleName, functionName, bufferArg, lengthArg) {
     Interceptor.attach(address, {
         onEnter(args) {
             const length = args[lengthArg].toInt32();
+            this.handle = args[0];
+            this.length = length;
             if (length > 0 && length <= 4096 && !args[bufferArg].isNull()) {
-                send({source: functionName, length: length}, args[bufferArg].readByteArray(length));
+                send({source: functionName, handle: this.handle.toString(), length: length}, args[bufferArg].readByteArray(length));
             }
+        },
+        onLeave(retval) {
+            send({source: functionName + ':return', handle: this.handle.toString(), retval: retval.toString()});
         }
     });
 }
@@ -38,6 +43,7 @@ Interceptor.attach(getFeature, {
         this.length = args[2].toInt32();
     },
     onLeave(retval) {
+        send({source: 'HidD_GetFeature:return', retval: retval.toString(), length: this.length});
         if (retval.toInt32() !== 0 && this.length > 0 && this.length <= 4096 && !this.buffer.isNull()) {
             send({source: 'HidD_GetFeature', length: this.length}, this.buffer.readByteArray(this.length));
         }
@@ -47,7 +53,40 @@ Interceptor.attach(getFeature, {
 hookBufferFunction('hid.dll', 'HidD_SetFeature', 1, 2);
 hookBufferFunction('hid.dll', 'HidD_SetOutputReport', 1, 2);
 hookBufferFunction('kernel32.dll', 'WriteFile', 1, 2);
-hookBufferFunction('kernel32.dll', 'DeviceIoControl', 2, 3);
+
+const deviceIoControl = Process.getModuleByName('kernel32.dll').getExportByName('DeviceIoControl');
+send({hooked: 'DeviceIoControl', address: deviceIoControl.toString()});
+Interceptor.attach(deviceIoControl, {
+    onEnter(args) {
+        this.handle = args[0];
+        this.code = args[1].toUInt32();
+        this.inBuffer = args[2];
+        this.inLength = args[3].toUInt32();
+        this.outBuffer = args[4];
+        this.outLength = args[5].toUInt32();
+        if (this.inLength > 0 && this.inLength <= 4096 && !this.inBuffer.isNull()) {
+            send({source: 'DeviceIoControl:in', handle: this.handle.toString(), code: this.code, length: this.inLength}, this.inBuffer.readByteArray(this.inLength));
+        } else {
+            send({source: 'DeviceIoControl:in', handle: this.handle.toString(), code: this.code, length: this.inLength});
+        }
+    },
+    onLeave(retval) {
+        send({source: 'DeviceIoControl:return', handle: this.handle.toString(), code: this.code, retval: retval.toString(), out_length: this.outLength});
+        if (retval.toInt32() !== 0 && this.outLength > 0 && this.outLength <= 4096 && !this.outBuffer.isNull()) {
+            send({source: 'DeviceIoControl:out', handle: this.handle.toString(), code: this.code, length: this.outLength}, this.outBuffer.readByteArray(this.outLength));
+        }
+    }
+});
+
+function hookCreateFile(name) {
+    let address;
+    try { address = Process.getModuleByName('kernel32.dll').getExportByName(name); } catch (_) { return; }
+    Interceptor.attach(address, {
+        onEnter(args) { this.path = args[0].readUtf16String(); },
+        onLeave(retval) { send({source: name, path: this.path, handle: retval.toString()}); }
+    });
+}
+hookCreateFile('CreateFileW');
 """
 
 
